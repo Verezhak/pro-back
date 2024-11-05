336cdb9edc170853ea8f43854b8e080208e2d72d
Verezhak <153289346+Verezhak@users.noreply.github.com>
2024-11-06 01:02:50 +0200

    add

--- src/validation/help.js
+++
@@ -1,6 +0,0 @@
-import Joi from 'joi';
-
-export const needHelpSchema = Joi.object({
-  email: Joi.string().email().required(),
-  message: Joi.string().required(),
-});
--- src/validation/users.js
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-
-export const updateUserSchema = Joi.object({
-    name: Joi.string().min(3).max(20),
-    email: Joi.string().email().min(3).max(20),
-    theme: Joi.string().valid('light', 'dark', 'violet'),
-    avatar: Joi.string(),
-});
