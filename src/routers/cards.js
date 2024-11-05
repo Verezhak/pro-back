c608b4e9e06cdf7a3ebcd962cf5dac327fadcfbd
Verezhak <153289346+Verezhak@users.noreply.github.com>
2024-11-06 01:01:25 +0200

    add lesia

--- src/routers/cards.js
+++ src/routers/cards.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import {
   getAllCardsController,
-  getCardByIdController,
+  getCardByIdController, 
   createCardController,
   updateCardController,
   deleteCardController,
-} from '../controllers/cards.js';
+} from '../controllers/cards.js';  
 import { validateBody } from '../middlewares/validateBody.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { cardSchema } from '../validation/cards.js';
@@ -13,7 +13,7 @@ import { cardSchema } from '../validation/cards.js';
 const cardsRouter = Router({ mergeParams: true });
 
 cardsRouter.get('/', getAllCardsController);
-cardsRouter.get('/:cardId', isValidId, getCardByIdController);
+cardsRouter.get('/:cardId', isValidId, getCardByIdController); 
 cardsRouter.post('/', validateBody(cardSchema), createCardController);
 cardsRouter.patch('/:cardId', isValidId, validateBody(cardSchema), updateCardController);
 cardsRouter.delete('/:cardId', isValidId, deleteCardController);
