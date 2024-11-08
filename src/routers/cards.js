a2d245b226f89966a91ecc52019b57b15dfd2425
Verezhak <153289346+Verezhak@users.noreply.github.com>
2024-11-08 18:54:56 +0200

    add work

--- src/routers/cards.js
+++ src/routers/cards.js
@@ -1,54 +1,10 @@
 
-// import { Router } from 'express';
-// import {
-//   getAllCardsController,
-//   getCardByIdController,
-//   createCardController,
-//   updateCardController,
-//   deleteCardController,
-//   moveCardController,
-// } from '../controllers/cards.js';
-// import { validateBody } from '../middlewares/validateBody.js';
-// import { isValidId } from '../middlewares/isValidId.js';
-// import { cardSchema, updateCardSchema } from '../validation/cards.js';
-
-// const cardsRouter = Router();
-
-// cardsRouter.get('/', getAllCardsController);
-// cardsRouter.get('/:cardId', isValidId('cardId'), getCardByIdController);
-// cardsRouter.post('/', validateBody(cardSchema), createCardController);
-// cardsRouter.patch(
-//   '/:cardId',
-//   isValidId('cardId'),
-//   validateBody(updateCardSchema),
-//   updateCardController,
-// );
-// cardsRouter.delete('/:cardId', isValidId('cardId'), deleteCardController);
-// cardsRouter.patch('/move/:cardId', moveCardController);
-
-// export default cardsRouter;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { Router } from 'express';
 
 import { validateBody } from '../middlewares/validateBody.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { cardSchema, updateCardSchema } from '../validation/cards.js';
 import { createCardController, deleteCardController, getAllCardsController, getCardByIdController, moveCardController, updateCardController } from '../controllers/cards.js';
-
-// import { authenticate } from '../middlewares/authenticate.js';
 
 const cardsRouter = Router({ mergeParams: true });
 
--- src/services/cards.js
+++ src/services/cards.js
@@ -1,49 +1,4 @@
 
-// import { Card } from '../db/cards.js';
-
-// export const getAllCards = async (boardId) => {
-//   return await Card.find({ boardId });
-// };
-
-// export const getCardById = async (cardId, boardId) => {
-//   return await Card.findOne({ _id: cardId, boardId });
-// };
-
-// export const createCard = async (payload) => {
-//   return await Card.create(payload);
-// };
-
-// export const updateCard = async (cardId, boardId, payload) => {
-//   const { newColumnId } = payload;
-
-
-//   if (newColumnId) {
-//     payload.columnId = newColumnId;
-//   }
-//   return await Card.findOneAndUpdate({ _id: cardId, boardId }, payload, { new: true });
-// };
-
-
-// export const deleteCard = async (cardId) => {
-//   return await Card.findByIdAndDelete(cardId);
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 import { Card } from '../db/cards.js';
